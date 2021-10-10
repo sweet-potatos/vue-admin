bb264be205e59629a67a86567b05d446c0ede424
xuzhuquan <xuzhuquan@dfl.com.cn>
2021-10-10 17:58:43 +0800

    feat: vue-简单配置vue.config.js
    --user=xuzhuquan

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: true,
+  productionSourceMap: false,
+  devServer: {
+    port: 8080,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
